5bfc2c9ab80ad7bc665b814a04fe0f149912b793
Abdulwahab <you@example.com>
2024-11-19 12:27:37 +0100

    added other prompt

--- hungry.py
+++ hungry.py
@@ -1,7 +1,9 @@
-"i am hungry"
+print("i am hungry")
 
 hungry = input("Are you hungry? ")
 if hungry == 'yes':
     print("go eat rice")
+    print("go eat beans")
+    print("go eat chicken")
 else:
     print('go do your homework')
